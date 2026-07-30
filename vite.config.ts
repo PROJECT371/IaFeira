45adfabc573ee2279b32510787d4b69024d3c524
PROJECT371 <projectspaulo@gmail.com>
2026-07-30 07:49:53 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/feirafood-ia/",
+  base: "/IaFeira/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
